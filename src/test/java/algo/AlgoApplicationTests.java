6ba910c379c575eaa726cede518f4787bf8152fb
ByeongGyu Jeon <cube9928@naver.com>
2025-02-25 17:49:36 +0900

    init

---
+++ src/test/java/algo/AlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package algo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
